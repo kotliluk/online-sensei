14712f6e43d7259b48f89ddc41d888c30b7f6061
Lukas Kotlik <kotliluk@fel.cvut.cz>
2021-11-23 22:05:31 +0100

    Add basic structure and dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { MainPage } from './pages/mainPage/MainPage'
 
-function App() {
+
+const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route path='/login'>
+        <div>Login</div>
+      </Route>
+      <Route path='*'>
+        <MainPage />
+      </Route>
+    </Switch>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/mainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
+import React from 'react'
+
+
+export const MainPage = (): JSX.Element => {
+
+  return (
+    <main className='main-page'>
+      Main Page
+    </main>
+  )
+}
