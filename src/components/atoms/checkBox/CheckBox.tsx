6239b1f1d6f2d4bfdbdba85e34215b49375ced82
Lukas Kotlik <kotliluk@fel.cvut.cz>
2021-12-05 23:27:26 +0100

    Styled checkbox in kumite fight.

--- src/components/atoms/checkBox/CheckBox.tsx
+++ src/components/atoms/checkBox/CheckBox.tsx
@@ -4,19 +4,20 @@ import './CheckBox.scss'
 
 
 interface CheckBoxProps {
+  className?: string
   checked: boolean
   onChange: (selected: boolean) => void
 }
 
 export const CheckBox = (props: CheckBoxProps): JSX.Element => {
-  const { checked, onChange } = props
+  const { className, checked, onChange } = props
 
   const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     onChange(e.target.checked)
   }, [onChange])
 
   return (
-    <span className={`__checkbox_container ${checked ? 'checked' : ''}`}>
+    <span className={`__checkbox_container ${checked ? 'checked' : ''} ${className ?? ''}`}>
       <span className='checkmark'/>
       <input type='checkbox' checked={checked} onChange={handleChange} />
     </span>
